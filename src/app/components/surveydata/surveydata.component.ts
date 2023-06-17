9985f384dbf90753dd06227c146261151c7e864e
Mohnish Raval <mohnishraval40@gmail.com>
2023-06-17 14:16:35 -0400

    Ag-grid basic impl

--- src/app/components/surveydata/surveydata.component.ts
+++ src/app/components/surveydata/surveydata.component.ts
@@ -1,30 +1,71 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { DataService } from 'src/app/services/data.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-surveydata',
   templateUrl: './surveydata.component.html',
   styleUrls: ['./surveydata.component.scss'],
 })
-export class SurveydataComponent implements OnInit {
+export class SurveydataComponent implements OnInit, OnDestroy {
+  surveyDataSubscriptions: Subscription[] = [];
   @ViewChild('dynamicContent') dynamicContent!: TemplateRef<any>;
 
   columnDefs: ColDef[] = [
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price' },
+    { field: 'FirstName' },
+    { field: 'LastName' },
+    { field: 'SurveyResult' },
+    { field: 'SurveyDate' },
   ];
 
   rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
+    {
+      FirstName: 'Mohnish',
+      LastName: 'Celica',
+      SurveyResult: 35000,
+      SurveyDate: '06-12-2023',
+    },
+    {
+      FirstName: 'Toyota',
+      LastName: 'Celica',
+      SurveyResult: 35000,
+      SurveyDate: '06-12-2023',
+    },
+    {
+      FirstName: 'Toyota',
+      LastName: 'Celica',
+      SurveyResult: 35000,
+      SurveyDate: '06-12-2023',
+    },
   ];
-  constructor(private modalService: ModalService) {}
+  constructor(
+    private modalService: ModalService,
+    private dataService: DataService
+  ) {}
 
   ngOnInit(): void {}
 
+  displayFormDetails() {
+    const displayFormDataSub = this.dataService
+      .fetchFormDetails()
+      .subscribe((formdata) => {
+        console.log(formdata);
+      });
+    this.surveyDataSubscriptions.push(displayFormDataSub);
+  }
+
   openModal() {
     this.modalService.openModal('SurveyData', this.dynamicContent);
+  }
+
+  ngOnDestroy(): void {
+    this.surveyDataSubscriptions.forEach((subi) => subi.unsubscribe());
   }
 }
